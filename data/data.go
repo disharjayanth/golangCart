cf07bef6d55cebaba067f89f550dcf4c3a12286f
disharjayantha <dishuj15@gmail.com>
2021-10-05 15:06:06 +0530

    inital commit

---
+++ data/data.go
@@ -0,0 +1,43 @@
+package data
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var collection *mongo.Collection
+var err error
+
+type Order struct {
+	OrderId     string            `bson:"order_id"`
+	Items       map[string]string `bson:"items"`
+	TotalAmount int               `bson:"total_amount"`
+}
+
+func init() {
+	password := os.Getenv("PASS")
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://user:" + password + "@cluster0.8ltsy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Println("Error connecting to client:", err)
+	}
+
+	err = client.Connect(context.Background())
+	if err != nil {
+		log.Printf("Error connecting to mongo server: %v", err)
+	}
+
+	collection = client.Database("ecommerce").Collection("order")
+}
+
+func (o *Order) Store() {
+	_, err = collection.InsertOne(context.Background(), o)
+	if err != nil {
+		fmt.Println("Error inserting order:", err)
+		return
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/disharjayanth/golangCart/data"
+)
+
+var temp *template.Template
+var err error
+
+func init() {
+	temp, err = template.ParseGlob("templates/*.html")
+	if err != nil {
+		fmt.Println("Error parsing glob of template files:", err)
+	}
+}
+
+func cartHandler(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "cart.html", nil)
+}
+
+func paymentHandler(w http.ResponseWriter, r *http.Request) {
+	order := data.Order{}
+	order.Items = make(map[string]string)
+
+	total := 0
+	for phone, cost := range r.URL.Query() {
+		fmt.Println(phone, cost[0])
+		n, err := strconv.Atoi(cost[0])
+		if err != nil {
+			fmt.Println("cannot parse string price: ", err)
+		}
+		total += n
+
+		order.OrderId = "1"
+		order.Items[phone] = cost[0]
+		order.TotalAmount = total
+
+		fmt.Println("Order:", order)
+	}
+
+	order.Store()
+	fmt.Println("Toal:", total)
+	http.Redirect(w, r, "/orderconfirmed", http.StatusSeeOther)
+}
+
+func orderConfirmed(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "orderPlaced.html", nil)
+}
+
+func main() {
+	server := http.Server{
+		Addr: ":" + os.Getenv("PORT"),
+		// Addr: "localhost:3000",
+	}
+
+	http.HandleFunc("/", cartHandler)
+	http.HandleFunc("/pay", paymentHandler)
+	http.HandleFunc("/orderconfirmed", orderConfirmed)
+
+	fmt.Println("Server listening @PORT: 3000")
+	server.ListenAndServe()
+}
